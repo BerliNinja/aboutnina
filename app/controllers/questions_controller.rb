734058744ccbcda4564491ec8685c32445b992d2
Nina Stache <nina.stache@absolventa.de>
2015-01-31 16:16:04 +0100

    add new, show, edit, create and update to questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,38 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
 
+def new
+  @question = Question.new
+end
+
+def show
+  @question = Question.find(params[:id])
+end
+
+def update
+end
+
+def edit
+end
+
+def create
+  @question = Question.new(permitted_params)
+  if @question.save
+    redirect_to @question, notice: 'ok!'
+  else
+    render :new
+  end
+end
+
+private
+
+def permitted_params
+  params.require(:question).permit(:title, :description, :timestamp)
+end
+
+def collection
+  @question ||= Question.all
+end
+
   helper_method :collection, :resource
 end
