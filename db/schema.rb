1d0a577cf98ddc947e400bc7fac4fbac0571c142
Nina Stache <nina.stache@absolventa.de>
2015-02-11 16:43:05 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204164850) do
+ActiveRecord::Schema.define(version: 20150211144622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "updated_at"
+    t.datetime "created_at"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150204164850) do
     t.string   "timestamp"
     t.datetime "updated_at"
     t.datetime "created_at"
+    t.string   "answer"
   end
 
   create_table "users", force: :cascade do |t|
